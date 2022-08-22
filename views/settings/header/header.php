05d204dee5fca057e58b17ff80fb7df7e852cf94
S.M. Rafiz <s.m.rafiz@gmail.com>
2022-08-22 22:08:51 +0600

    Some changes in SASS

--- views/settings/header/header.php
+++ views/settings/header/header.php
@@ -11,8 +11,15 @@
 ?>
 
 <div class="header-banner">
-	<h1>Klaro Consent Manager</h1>
-	<div class="logo-container">
-		<img src="<?php echo esc_url( sd_klaro_consent()->getData()['plugin_uri'] ); ?>/assets/images/merryll-logo.png" alt="merryll">
+	<div class="logo-container sigma-d-flex sigma-align-center">
+		<img src="<?php echo esc_url( sd_klaro_consent()->getData()['plugin_uri'] ); ?>/assets/images/sigma-klaro-logo.svg" alt="Sigma-Klaro-Consent-Manager">
+		<h1><?php esc_html_e( 'Klaro Consent Manager', 'klaro-consent' ); ?></h1>
+	</div>
+	<div class="navbar-container">
+		<nav id="header-nav">
+			<ul>
+				<li><?php esc_html_e( 'Dashboard', 'klaro-consent' ); ?></li>
+			</ul>
+		</nav>
 	</div>
 </div>
